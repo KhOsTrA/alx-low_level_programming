08c4612d5d28cadb3b04370c637dbc233e0acf01
KhOsTrA <khostra@msn.com>
2022-12-07 08:39:15 -0800

    Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,41 @@
-i#include "main.h"
+#include "main.h"
 /**
-  * create_file - creates a file.
-  * @filename: pointer to filename.
-  * @text_content: string to be written to file.
-  *
-  * Return:  1 on success -1 on failure.
-  */
+ * create_file - create a file.
+ * @filename: name of the file to create.
+ * @text_content: text to write inside the file.
+ *
+ * Return: 1 if the file was created, -1 otherwise.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i, wr;
+	int fd_open, fd_write;
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
+	fd_open = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (fd_open == -1)
 		return (-1);
-
-	if (text_content)
+	if (text_content != NULL)
 	{
-		i = 0;
-		while (text_content[i])
-			i++;
-		wr = write(file, text_content, i);
-		if (wr != i)
+		fd_write = write(fd_open, text_content, _strlen(text_content));
+		if (fd_write == -1)
 			return (-1);
 	}
-
-	close(file);
+	close(fd_open);
 	return (1);
+}
+
+/**
+ * _strlen - calculate the lenght of a string.
+ * @str: array of characters.
+ *
+ * Return: lenght of the string.
+ */
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (*str++)
+		count++;
+	return (count);
 }
